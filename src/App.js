688db6d66aba3273a606e6741bd1d6b0d490811a
Cherrie Awang <caflores31@gmail.com>
2021-05-25 21:41:20 -0700

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React,{Component} from 'react';
-import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
- import Sidebar from './components/sidebar/Sidebar';
-import About from './components/about/About';
-import Education from './components/Education/Education';
-import Interest from './components/skills/Interest'
-import CarouselImages from './components/carousel/CarouselImages.js'
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
+function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
 
-class App extends Component {
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-  render(){
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <Router>
-      <div className="App">
-          <div className="side">
-            <nav className="navbar side navbar-expand-lg navbar-light p-0" >
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" style={{zIndex:'1'}}>
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <Sidebar />
-              </div>
-            </nav>  
-          </div>
-            <div className="main">
-              <div>
-
-                 <CarouselImages />
-              </div>
-                 <About />
-                <Interest />
-                <Education />
-            </div>
-      </div>
-   </Router>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+    </div>
   );
-  }
 }
 
 export default App;
